e9b2a61d0805865e618ffa300e9aae938852b6c9
DiegoDiazRodri <110435602+DiegoDiazRodri@users.noreply.github.com>
2024-10-12 08:35:41 -0500

    py

---
+++ DetallePersona.py
@@ -0,0 +1,10 @@
+""" Creacion de variables para la descripcion de persona """
+
+Altura = 1.56,  #altura
+Name = "Diego Alejandro Diaz Rodriguez", #nombre
+Edad = 21, #edad
+Mascota = True, #mascota
+ListaAmigos = ["Ximena", "Juan", "Nicolas"], #lista de amigos
+
+print("Soy", Name, "mi estatura es" ,Altura,"tengo", Edad ,"años","\n",
+      "Tengo mascota",Mascota, "y mis amigos son", ListaAmigos)
---
+++ EdadMayorPersona.py
@@ -0,0 +1,7 @@
+pedro = 18
+juan = 20
+
+if pedro > juan:
+    print("Pedro es mayor")
+else:
+    print("Juan es mayor")
